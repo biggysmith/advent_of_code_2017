8b87c1ed195150e3428e36f0aad6c00d52626f69
Scott Smith <ssmith@tmvse.com>
2021-12-30 09:48:50 +0000

    day24

--- src/day24/day24.cpp
+++ src/day24/day24.cpp
@@ -9,16 +9,13 @@
 #include <set>
 #include <map>
 #include <queue>
+#include "timer.hpp"
 
 struct component_t{
     int port0, port1;
     int used_port = -1;
 };
-
-bool operator==(const component_t& a,const component_t& b){
-    return a.port0 == b.port0 && a.port1 == b.port1;
-}
-                
+            
 std::vector<component_t> load_input(const std::string& file){
     std::vector<component_t> ret;
     std::ifstream fs(file);
@@ -30,93 +27,67 @@ std::vector<component_t> load_input(const std::string& file){
         ss >> com.port0 >> c >> com.port1;
         ret.push_back(com);
     }   
-
     return ret;
 }
 
-void find_bridge(const std::vector<component_t>& components, const std::vector<component_t>& path, std::vector<std::vector<component_t>>& final_paths){
+int com_strength(const component_t& com){
+    return com.port0 + com.port1;
+}
 
-    auto& last = path.back();
-    for(auto& com : components){
-        if(std::find(path.begin(), path.end(), com) == path.end()){ // not present in path
+void find_bridge(
+    const std::vector<component_t>& components, 
+    const component_t& last,
+    int current_strength,
+    int& max_strength,
+    int current_depth,
+    int& max_depth,
+    int& longest_max_strength
+){
+    for(int c=0; c<components.size(); ++c)
+    {
+        auto& com = components[c];
 
-            auto extend = [&](int use){
-                auto new_path = path;
-                new_path.push_back({ com.port0, com.port1, use });
-                final_paths.push_back(new_path);
-                find_bridge(components,new_path,final_paths);
-            };
+        auto extend = [&](int use){
+            auto new_coms = components;
+            new_coms.erase(new_coms.begin()+c);
 
-            if(last.used_port == 0){
-                if(last.port1 == com.port0){
-                    extend(0);
-                }else if(last.port1 == com.port1){
-                    extend(1);
-                }
-            }
+            int new_strength = current_strength + com_strength(com);
+            max_strength = std::max(max_strength, new_strength);
 
-            if(last.used_port == 1){
-                if(last.port0 == com.port0){
-                    extend(0);
-                }else if(last.port0 == com.port1){
-                    extend(1);
-                }
-            }
+            find_bridge(new_coms, { com.port0, com.port1, use }, new_strength, max_strength, current_depth + 1, max_depth, longest_max_strength);
+        };
 
+        if((last.used_port == 0 && last.port1 == com.port0) || (last.used_port == 1 && last.port0 == com.port0)){
+            extend(0);
+        }else if((last.used_port == 0 && last.port1 == com.port1) || (last.used_port == 1 && last.port0 == com.port1)){
+            extend(1);
         }
     }
 
-}
-
-void print_path(const std::vector<component_t>& path){
-    for(auto& com : path){
-        std::cout << com.port0 << "/" << com.port1 << "--";
+    if(current_depth > max_depth){
+        max_depth = current_depth;
+        longest_max_strength = current_strength;
     }
-    std::cout << std::endl;
-}
-
-int strength(const std::vector<component_t>& path){
-    int sum = 0;
-    for(auto& com : path){
-        sum += com.port0 + com.port1;
+    else if(current_depth == max_depth){
+        longest_max_strength = std::max(longest_max_strength, current_strength);
     }
-    return sum;
 }
 
 auto process(const std::vector<component_t>& components)
 {
-    std::vector<std::vector<component_t>> final_paths;
-
-    for(auto& com : components){
-        if(com.port0 == 0){
-            std::vector<component_t> path { { com.port0, com.port1, 0 } };
-            final_paths.push_back(path);
-            find_bridge(components, path, final_paths);
-        }
-    }
-
+    int max_depth = 0;
     int max_strength = 0;
-    for(auto& path : final_paths){
-        max_strength = std::max(max_strength, strength(path));
-    }
-
-    std::vector<std::vector<component_t>> longest_paths;
-    int max_length = 0;
-    for(auto& path : final_paths){
-        if(path.size() > max_length){
-            max_length = (int)path.size();
-            longest_paths.clear();
-        }else if(path.size() == max_length){
-            longest_paths.push_back(path);
+    int longest_max_strength = 0;
+    for(int c=0; c<components.size(); ++c){
+        auto& com = components[c];
+        if(com.port0 == 0){
+            auto new_coms = components;
+            new_coms.erase(new_coms.begin()+c);
+            find_bridge(new_coms, { com.port0, com.port1, 0 }, com_strength(com), max_strength, 0, max_depth, longest_max_strength);
         }
     }
 
-    int max_strength2 = 0;
-    for(auto& path : longest_paths){
-        max_strength2 = std::max(max_strength2, strength(path));
-    }
-
-    return std::make_pair(max_strength, max_strength2);
+    return std::make_pair(max_strength, longest_max_strength);
 }
 
 void main()
